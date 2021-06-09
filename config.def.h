ab83085e6433f871d89aca61c21ff2f8152c90eb
Moritz Müller <Moritz.Mueller@mixed-mode.de>
2021-06-09 21:01:01 +0200

    change to kitty terminal

--- config.def.h
+++ config.def.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st","-f", "DejaVu Sans Mono-11", "-e", "tmux", "new-session", "-t", "main", NULL };
+static const char *termcmd[]  = { "kitty", "tmux", "new-session", "-t", "main", NULL };
 static const char *lockcmd[]  = { "slock", "systemctl", "suspend", NULL};
 /*First arg only serves to match against key in rules*/
 
